819139afe268b946beb7bd87383efdc84676b46e
shvixxl <shvix@pm.me>
2022-02-19 23:54:43 +0300

    Remove abandoned dependency package pytest-env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,11 @@
 """Pytest fixtures."""
+
+import os
+
+from pytest import Config
+
+
+def pytest_configure(config: Config) -> None:
+    """Configures pytest before testing."""
+
+    os.environ['BOT_TOKEN'] = 'token'
