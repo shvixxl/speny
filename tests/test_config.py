2259d61e8ecfa00852c040e7fe0282e7ff9ea58b
shvixxl <shvix@pm.me>
2022-02-19 23:41:04 +0300

    Add simple test to pass builds

---
+++ tests/test_config.py
@@ -0,0 +1,16 @@
+"""Tests configuration module."""
+
+from aiogram.utils.backoff import BackoffConfig
+
+from speny.config import settings, Settings
+
+
+def test_settings():
+    """Tests if settings object."""
+
+    assert isinstance(settings, Settings)
+
+    assert isinstance(settings.BOT_TOKEN, str)
+    assert isinstance(settings.LOGGING_LEVEL, int)
+    assert isinstance(settings.POLLING_TIMEOUT, int)
+    assert isinstance(settings.BACKOFF_CONFIG, BackoffConfig)
